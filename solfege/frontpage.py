1ee9b87bccb5b4195a70bfca6a5dda59cd3a509e
Paulo Victor <paulovictorjava2016@gmail.com>
2016-04-23 01:46:48 -0300

    frontpage.py
    indenting updated solfege / frontpage.py

--- solfege/frontpage.py
+++ solfege/frontpage.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from __future__ import absolute_import
 from __future__ import with_statement
 
@@ -35,7 +34,6 @@ class FrontPageException(Exception):
 class OldFormatException(FrontPageException):
     pass
 
-
 def mk_rel(filename, save_location):
     assert os.path.isabs(filename) or filename.startswith(lessonfile.solfege_uri), filename
     if filename.startswith(save_location):
@@ -52,22 +50,27 @@ def escape(s):
 
 def get_front_pages_list(debug):
     files = []
+
     def is_frontpage(fn):
             # no subdirs
             if not os.path.isfile(fn):
                 return False
             # filter out bzr revert backups
+
             if fn.endswith("~"):
                 return False
+
             if os.path.split(fn)[1] == u"Makefile":
                 return False
             return True
+
     def add_subdir(subdir):
         if os.path.isdir(subdir):
             v = [os.path.join(subdir, fn) for fn in os.listdir(subdir)]
             return [x for x in v if is_frontpage(x)]
         return []
     files = [fn for fn in glob.glob(os.path.join(u"exercises", "*", "*"))
+
              if is_frontpage(fn)]
     # The next line is for pre 3.15 compat. We used to place learning
     # trees there.
@@ -75,7 +78,9 @@ def get_front_pages_list(debug):
     # This is the recommended place to save front page files
     files.extend([
         fn for fn in glob.glob(os.path.join(filesystem.user_data(), u"exercises", "*", "*"))
+
         if is_frontpage(fn)])
+
     if not debug:
         try:
             files.remove(os.path.join(lessonfile.exercises_dir, 'debugtree.txt'))
@@ -84,23 +89,28 @@ def get_front_pages_list(debug):
             pass
     return files
 
-
 class _TreeCommon(list):
+
     def __init__(self, items=[]):
         if isinstance(items, _TreeCommon):
             list.__init__(self, [items])
+
         else:
             list.__init__(self, items)
+
     def dump(self, stream, level=0):
         print >> stream, "%s%s([" % (" " * level, self.__class__.__name__)
         self.dump_children(stream, level)
         print >> stream, "%s ])," % (" " * level)
+
     def dump_children(self, stream, level):
         for child in self:
             if isinstance(child, _TreeCommon):
                 child.dump(stream, level + 1)
+
             else:
                 print >> stream, "%su'%s'," % (" " * (level + 1), escape(child))
+
     def iterate_filenames(self):
         """
         Yield the filenames of each lesson that has been added to this
@@ -110,8 +120,10 @@ class _TreeCommon(list):
             if isinstance(child, _TreeCommon):
                 for c in child.iterate_filenames():
                     yield c
+
             if isinstance(child, (str, unicode)):
                 yield child
+
     def get_use_dict(self):
         """
         Return a dict where the keys are the filename of lessons used
@@ -122,13 +134,16 @@ class _TreeCommon(list):
         for filename in self.iterate_filenames():
             retval[filename] = retval.get(filename, 0) + 1
         return retval
+
     def iterate_topics_for_file(self, filename):
         for child in self:
             if isinstance(child, (Page, Column, LinkList)):
                 for c in child.iterate_topics_for_file(filename):
                     yield c
+
             elif child == filename:
                 yield self.m_name
+
     def iterate_flattened(self):
         for child in self:
             yield child
@@ -136,6 +151,7 @@ class _TreeCommon(list):
                 for subchild in child.iterate_flattened():
                     yield subchild
     @staticmethod
+
     def tests_in_sub(sub):
         if isinstance(sub, unicode):
             try:
@@ -143,21 +159,26 @@ class _TreeCommon(list):
             except solfege.lessonfile.infocache.FileNotFound:
                 return False
         for child in sub:
+
             if _TreeCommon.tests_in_sub(child):
                 return True
         return False
+
     def foreach_file(self, callback, data=None):
         try:
             for idx, child in enumerate(self):
                 if hasattr(self[idx], 'foreach_file'):
                     self[idx].foreach_file(callback, data)
+
                 else:
                     if data:
                         self[idx] = callback(self[idx], data)
+
                     else:
                         self[idx] = callback(self[idx])
         except TypeError:
             pass
+
     def get_topic_of_lesson_file(self, filename):
         """
         Return the title of the first topic where filename is linked.
@@ -167,10 +188,12 @@ class _TreeCommon(list):
             return x
 
 class _NamedTreeCommon(_TreeCommon):
+
     def __init__(self, name=u'', listitems=[]):
         assert isinstance(name, unicode)
         _TreeCommon.__init__(self, listitems)
         self.m_name = name
+
     def dump(self, stream, level=0):
         print >> stream, "%s%s(_(u'%s'), [" % (" " * level, self.__class__.__name__, escape(self.m_name))
         self.dump_children(stream, level)
@@ -180,26 +203,33 @@ class LinkList(_NamedTreeCommon):
     """
     A list of links leading to exercises or new pages.
     """
+
     def append(self, item):
         assert isinstance(item, (str, unicode, Page))
         super(LinkList, self).append(item)
+
     def __str__(self):
         return "LinkList(_('%s')# len: %i)" % (self.m_name, len(self))
 
 class Column(_TreeCommon):
+
     def add_linklist(self, heading):
         self.append(LinkList(heading))
         return self[-1]
+
     def __str__(self):
         return "Column(#len: %i)" % len(self)
 
 class Page(_NamedTreeCommon):
+
     def __init__(self, name=u'', listitems=[]):
         assert isinstance(name, unicode)
         _NamedTreeCommon.__init__(self, name, listitems)
         self.m_modified = False
+
     def __repr__(self):
         return "<Page name=%s>" % self.m_name
+
     def get(self, path):
         """
         Return the element pointed to by path.
@@ -208,6 +238,7 @@ class Page(_NamedTreeCommon):
         for idx in path[1:]:
             elem = elem[idx]
         return elem
+
     def is_empty(self):
         """
         Return True if all columns are empty or only contain empty sections.
@@ -219,17 +250,21 @@ class Page(_NamedTreeCommon):
         return True
 
 class Paragraph(object):
+
     def __init__(self, text):
         self.m_text = text
 
 class FileHeader(_TreeCommon):
+
     def __init__(self, version, page):
         _TreeCommon.__init__(self, [page])
         self.m_version = version
+
     def dump(self, stream, level=0):
         print >> stream, "%s%s(%s, " % (" " * level, self.__class__.__name__, self.m_version)
         self.dump_children(stream, level)
         print >> stream, "%s )" % (" " * level)
+
     def save_file(self, filename):
         """
         Rules:
@@ -247,7 +282,6 @@ class FileHeader(_TreeCommon):
         f.close()
         self.foreach_file(mk_abs, save_location)
 
-
 def may_be_frontpage(filename):
     """
     Return True if we think this is a front page file.
@@ -264,12 +298,12 @@ def may_be_frontpage(filename):
             # '{' is 3.14
             # 'Page' is bzr branch 3.15.x
             # 'FileHeader is 3.15.3 (or was it .2?)
+
             if s.startswith(('{', 'Page', 'FileHeader')):
                 return True
             logging.debug("my_be_frontpage(%s) => False" % filename)
             return False
         return False
-
 
 def parse_tree(s, C_locale=False):
     """
@@ -278,18 +312,21 @@ def parse_tree(s, C_locale=False):
     new format files.
     """
     if C_locale:
+
         def ifu(s):
             # We do the isinstance check because this way we can handle
             # people manually editing the data file and adding strings that
             # are plain ascii, but not marked as unicode ( u'' )
             return s if isinstance(s, unicode) else unicode(s)
         ifunc = ifu
+
     else:
         # The headings and links to exercises or pages should not have
         # accels (defined in gtk+ by underlines "_") or ellipsis "…" that
         # are used in menu items and command button labels. So we remove
         # them, if found, in the string. This way we can reuse strings
         # with accels that are translated, saving translator work.
+
         def _s(s):
             return _(s).replace("_", "").replace(u"…", "")
         ifunc = _s
@@ -308,6 +345,7 @@ def parse_tree(s, C_locale=False):
         raise FrontPageException(str(e))
     if isinstance(ret, FileHeader):
         return ret[0]
+
     else:
         raise OldFormatException()
 
@@ -316,4 +354,3 @@ def load_tree(fn, C_locale=False):
     # We store all files by absolute filename or solfege: uri internally
     ret.foreach_file(mk_abs, os.path.split(fn)[0] + os.sep)
     return ret
-
